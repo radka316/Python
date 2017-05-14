010a2029b08ba71856dd6eb9a5d79825b902e48d
Radka Bodnárová <bodnarad@fit.cvut.cz>
2017-05-14 22:02:18 +0200

    functions to rotate and invert img

--- semestralka.py
+++ semestralka.py
@@ -43,10 +43,44 @@ def original():
     panel.image= img
     act= resized
 
+def rotate():
+    global act, resized
+    if act=="":
+        img2 = resized
+    else:
+        img2= act
+    data= np.asarray(img2)
+    data_out=np.rot90(data)
+    data_out=np.asarray(data_out, dtype=np.uint8)
+    img_out=Image.fromarray(data_out)
+    act= img_out
+    img_new = ImageTk.PhotoImage(img_out)
+    panel.configure(image = img_new)
+    panel.image= img_new
+
+def invert():
+    global act, resized
+    if act=="":
+        img3 = resized
+    else:
+        img3= act
+    data= np.asarray(img3)
+    try:
+        data_out = 255 - data
+        img_out = Image.fromarray(data_out)
+        img_new = ImageTk.PhotoImage(img_out)
+    except:
+        return 0
+    act=img_out
+    panel.configure(image = img_new)
+    panel.image= img_new
+
 #tlacidla
 original=Button(main,text="Original", command=original, width=10)
 original.grid(row=1, column=0)
 black=Button(main,text="Odstíny šedi", command= black_white, width=10)
 black.grid(row=2, column=0)
-rotate_l=Button(main,text="Otočit", width=10)
+rotate_l=Button(main,text="Otočit", command= rotate, width=10)
 rotate_l.grid(row=3, column=0)
+neg=Button(main,text="Inverzní obraz", command=invert, width=10)
+neg.grid(row=5, column=0)
