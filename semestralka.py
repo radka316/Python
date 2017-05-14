7ff68b5577fc5bdd85701d9947f9537a0e0769a5
Radka Bodnárová <bodnarad@fit.cvut.cz>
2017-05-14 20:27:23 +0200

    open and resize image

--- semestralka.py
+++ semestralka.py
@@ -5,3 +5,12 @@ import numpy as np
 
 main=Tk()
 main.title("Uprava obrazku")
+path = "pizza.jpg" #zadaj cestu k obrazku
+
+origo = Image.open(path)
+width= (500 / float(origo.size[0]))
+height= int((float(origo.size[1]) * float(width)))
+resized= origo.resize((500, height), PIL.Image.ANTIALIAS)
+imgg=ImageTk.PhotoImage(resized)
+panel = Label(main, image = imgg)
+panel.grid(row=0, column=0)
